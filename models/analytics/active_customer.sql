989bb4bf5672d7a039b71aea5f1f68437ddc1c4e
Adetutu14 <adetutuadejuyigbe@gmail.com>
2024-06-07 21:31:39 +0100

    add dbt project files

---
+++ models/analytics/active_customer.sql
@@ -0,0 +1,11 @@
+with active_customers as (
+    select * from {{ source('dvdrental', 'customer')}}
+    where active = 1
+),
+final as (
+    select first_name, 
+    last_name, 
+    active
+    from active_customers
+)
+select * from final
